77acce263c55d7ea161d7008e14bdef9b3b90176
bahelms <jimbonkgit@gmail.com>
2014-03-20 07:26:55 -0400

    adds editing account tests and sets timeoutable at 2 hours

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -117,7 +117,7 @@ Devise.setup do |config|
   # initial account confirmation) to be applied. Requires additional unconfirmed_email
   # db field (see migrations). Until confirmed new email is stored in
   # unconfirmed email column, and copied to email column on successful confirmation.
-  config.reconfirmable = true
+  # config.reconfirmable = true
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [ :email ]
@@ -145,7 +145,7 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  # config.timeout_in = 30.minutes
+  config.timeout_in = 2.hours
 
   # If true, expires auth token on session timeout.
   # config.expire_auth_token_on_timeout = false
--- spec/support/authentication.rb
+++ spec/support/authentication.rb
@@ -1,9 +1,9 @@
 def signup_with(options)
+  confirm = options[:confirm] ? options[:confirm] : options[:password]
+
   fill_in "Username", with: "Bob"
   fill_in "Email", with: options[:email]
   fill_in "Password", with: options[:password]
-
-  confirm = options[:confirm] ? options[:confirm] : options[:password]
   fill_in "Password confirmation", with: confirm
   click_button "Sign Up"
 end
@@ -12,4 +12,14 @@ def sign_in_with(options)
   fill_in "Email", with: options[:email]
   fill_in "Password", with: options[:password]
   click_button "Sign In"
+end
+
+def update_user(options)
+  confirm = options[:confirm] ? options[:confirm] : options[:new_password]
+
+  fill_in "Email", with: options[:email]
+  fill_in "Password", with: options[:new_password]
+  fill_in "Password confirmation", with: confirm
+  fill_in "Current password", with: options[:password]
+  click_button "Update"
 end
